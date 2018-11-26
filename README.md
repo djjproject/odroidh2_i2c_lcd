d99103a2772c1e285028bd7009e092bba9326f03
Lee Seungchul <john.lee@hardkernel.com>
2018-11-26 09:25:09 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# i2c_20x4_lcd
+
+## Where to buy
+* https://www.hardkernel.com/shop/i2c-lcd-module/
+
+## Tutorials
+* https://wiki.odroid.com/odroid-h2/application_note/i2c_20x4_lcd
+
+## Based on
+* https://github.com/pimatic/pimatic/issues/271
+* http://www.gejanssen.com/howto/i2c_display_2004_raspberrypi/index.html
+* https://github.com/sweetpi/python-i2c-lcd
---
+++ hello_world.py
@@ -0,0 +1,17 @@
+import lcddriver as lcd
+from time import time
+from time import sleep
+from datetime import datetime
+
+lcd = lcd.lcd()
+
+#lcd.display_string("11111111111111111111222222222222222222223333333333333333333344444444444444444444", 1)
+lcd.display_string("20x4 LCD Example", 1)
+lcd.display_string("Hello ODROID", 2)
+
+while True:
+  dateString = datetime.now().strftime('%b %d %y')
+  timeString = datetime.now().strftime('%H:%M:%S')
+  lcd.display_string(dateString, 3)
+  lcd.display_string(timeString, 4)
+  sleep(1)
